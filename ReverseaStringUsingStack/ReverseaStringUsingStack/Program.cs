0804dda982864fbe0204801e3ca76345fff1865c
Antonia Ivanova <antoniqmml@mail.bg>
2025-10-01 16:15:53 +0300

    Add files via upload

---
+++ ReverseaStringUsingStack/ReverseaStringUsingStack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static string ReverseString(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            stack.Push(c);
+        }
+
+        char[] reversed = new char[input.Length];
+        int index = 0;
+
+        while (stack.Count > 0)
+        {
+            reversed[index++] = stack.Pop();
+        }
+
+        return new string(reversed);
+    }
+
+    static void Main()
+    {
+        string text = "Interview";
+        string reversed = ReverseString(text);
+        Console.WriteLine($"Original: {text}");
+        Console.WriteLine($"Reversed: {reversed}");
+    }
+}
